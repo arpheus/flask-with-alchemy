cc62699efc2284326fbe92be184d49e5024f5deb
Aurélien PETIT <arpheus@free.fr>
2019-09-26 16:24:48 +0200

    let's trigger travis

--- wsgi.py
+++ wsgi.py
@@ -1,27 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
-
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow # Order is important here!
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
 from models import Product
-from schemas import products_schema
+from schemas import products_schema, product_schema
 
 @app.route('/hello')
 def hello():
     return "Hello World!"
 
 @app.route('/products')
-def products():
+def read_products():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return products_schema.jsonify(products)
 
 @app.route('/products/<int:id>')
-def getOneProduct(id):
+def read_product(id):
     #query db
     to_return = db.session.query(Product).get(id)
     #serialize and return
@@ -33,22 +33,62 @@ def create_product():
     #get json body
     json = request.get_json()
     #validate parameters in json
+    if type(json) is dict:
+        new_product = process_create_poduct(json)
+        return product_schema.jsonify(new_product),201
+    elif type(json) is list:
+        #bulk create
+        created_products = []
+        for item in json:
+            created_products.append(process_create_poduct(item))
+        return products_schema.jsonify(created_products),201
+    else:
+        abort(422)
+
+def process_create_poduct(json):
+    # if not "name" -> 400
+
     if "name" in list(json) and json["name"] != None:
+        desc = None
+        if "description" in list(json):
+            desc = json['description']
+
+        # filtered_json = { key, value for key, value in json if key in ['name', 'description'] }
+        # Product(**filtered_json)
+
         #create product object
         new_product = Product()
-        new_product.name = json["name"]
+        #product = Product(**filtered_json)
+        Product(name=json['name'], description=desc)
         #persist in db
         db.session.add(new_product)
         db.session.commit()
+        return new_product
 
 @app.route('/products/<int:id>', methods=['DELETE'])
-def delete_object(id):
-    num_row = db.session.query(Product).get(id).delete()
-    db.session.commit()
-    return jsonify(num_row)
+def delete_product(id):
+    to_delete = db.session.query(Product).get(id)
+    if to_delete != None:
+        num_row = db.session.delete(to_delete)
+        db.session.commit()
+    return '', 204
+
+@app.route('/products', methods=['DELETE'])
+def bulk_delete_product():
+    #get json list
+    json = request.get_json()
+    if "ids" in list(json) and type(json['ids']) is list:
+        for id_to_delete in json['ids']:
+            to_delete = db.session.query(Product).get(id_to_delete)
+            if to_delete != None:
+                num_row = db.session.delete(to_delete)
+        db.session.commit()
+        return '', 204
+    else:
+        abort(422)
 
 @app.route('/products/<int:id>', methods=['PATCH'])
-def change_object(id):
+def update_product(id):
     #get json with modifs
     json = request.get_json()
     modified_product = db.session.query(Product).get(id)
@@ -60,3 +100,4 @@ def change_object(id):
     #persist in db
     db.session.add(modified_product)
     db.session.commit()
+    return '', 204
