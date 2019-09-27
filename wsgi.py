e594c20a23f7ec40084a2f1b1f698d9963fc57a7
Aurélien PETIT <arpheus@free.fr>
2019-09-27 11:30:19 +0200

    flask templating

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, abort
+from flask import render_template
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,9 +12,15 @@ ma = Marshmallow(app)
 from models import Product
 from schemas import products_schema, product_schema
 
-@app.route('/hello')
-def hello():
-    return "Hello World!"
+@app.route('/')
+def home():
+    products = db.session.query(Product).all()
+    return render_template('home.html', products=products)
+
+@app.route('/<int:id>')
+def product_html(id):
+    product = db.session.query(Product).get(id)
+    return render_template('product.html', product=product)
 
 @app.route('/products')
 def read_products():
